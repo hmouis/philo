191f610e355bddf0062e707afeb43c429f0ac7a3
hmouis <houdayfamouis@gmail.com>
2025-07-09 02:05:47 +0100

    free all

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmouis <hmouis@1337.ma>                    +#+  +:+       +#+        */
+/*   By: hmouis <hmouis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 15:38:11 by hmouis            #+#    #+#             */
-/*   Updated: 2025/07/03 15:41:09 by hmouis           ###   ########.fr       */
+/*   Updated: 2025/07/09 02:04:34 by hmouis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@ int	main(int ac, char **av)
 	if (!pars_args(ac, av))
 		printf("Error: Invalide args\n");
 	else
+	{
 		init_philos(&table, av);
+		free(table.forks);
+		free(table.philo);
+		free(table.all_thread);
+		free(table.t_th);
+	}
 	return (0);
 }
